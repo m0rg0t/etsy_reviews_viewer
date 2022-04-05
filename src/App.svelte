e65de894687598e6ba55d859af13845e21d43d24
Anton Lenev <m0rg0t.Anton@gmail.com>
2022-04-06 01:03:09 +0300

    Upload json file option added

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,7 @@
 
     export let reviews: Array<IReviewItem> = [];
     export let reviewsRawText: string = `[{"reviewer":"Amy","date_reviewed":"03/04/2022","star_rating":5,"message":"Great product, took its time arriving but in adequate time for the distance it was travelling. It’s made well. I’ve worn it in my hair for a couple of days and it does not budge! Great quality products for a quick and easy up dip that’s a little edgy. I’ll be recommending this shop again.","order_id":2370958767},{"reviewer":"Cécile","date_reviewed":"02/24/2022","star_rating":5,"message":"Very nice and very pleasant seller, thanks!","order_id":2359711045},{"reviewer":"Jaynie","date_reviewed":"02/07/2022","star_rating":5,"message":"Beautiful little item, excellent seller communication, in contact all through the buying and delivery process. Perfect!","order_id":2343770675},{"reviewer":"Rose","date_reviewed":"12/14/2021","star_rating":5,"message":"Wonderful and thoughtful seller. The item is very cute and works well!","order_id":2272118499}]`;
+    //export let reviewsRawText: string = `[]`;
     reviews = JSON.parse(reviewsRawText)
 
     const onDownloadCsv = () => {
@@ -16,13 +17,26 @@
     const onChangeReviewsTextarea = () => {
         reviews = JSON.parse(reviewsRawText)
     }
+
+    const uploadJson = async (e: any) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = async (e: any) => {
+            reviews = JSON.parse(e.target.result);
+            reviewsRawText = JSON.stringify(reviews);
+        }
+    }
 </script>
 
 <header>
     <h1>Etsy review prettier</h1>
     <div class="input-reviews">
-        <label>Please enter your json</label>
-        <textarea on:change={onChangeReviewsTextarea} class="reviews-json" bind:value={reviewsRawText}></textarea>
+        <label for="json">Please enter your json</label>
+        <textarea name="json" id="json" on:change={onChangeReviewsTextarea} class="reviews-json" bind:value={reviewsRawText}></textarea>
+        <label for="upload_json">Or upload a json file</label>
+        <input id="upload_json" name="upload_json" type="file" accept=".json" on:change={uploadJson}/>
+        <hr/>
         <button on:click={onDownloadCsv}>Download CSV (for Excel)</button>
     </div>
 </header>
